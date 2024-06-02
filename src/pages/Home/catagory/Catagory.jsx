12055a1912b6d48869df45f429b72816ea35cc57
EmranHossain <emran.shuvo.dev@gmail.com>
2024-06-02 16:12:47 +0600

    added category image

--- src/pages/Home/catagory/Catagory.jsx
+++ src/pages/Home/catagory/Catagory.jsx
@@ -1,10 +1,5 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
-import slide1 from '../../../../public/assets/home/slide1.jpg'
-import slide2 from '../../../../public/assets/home/slide2.jpg'
-import slide3 from '../../../../public/assets/home/slide3.jpg'
-import slide4 from '../../../../public/assets/home/slide4.jpg'
-import slide5 from '../../../../public/assets/home/slide5.jpg'
 import 'swiper/css';
 import 'swiper/css/pagination';
 import SectionTitles from '../../../components/SectionTitles/SectionTitles';
@@ -13,7 +8,7 @@ const Catagory = () => {
     return (
         <section>
             <SectionTitles
-                subHeading={"From 10.00am to 10.00pm"}
+                subHeading={"All you need"}
                 heading={"Order online"}
             >
 
@@ -28,16 +23,34 @@ const Catagory = () => {
                 modules={[Pagination]}
                 className="mySwiper mb-24"
             >
-                <SwiperSlide className=''><img src={slide1}
+                <SwiperSlide>
+                    <img  className='h-[50vh]' src={`https://i.ibb.co/HnVyCK5/oriol-pascual-l5-VCmq-Qb-P-g-unsplash.jpg`}
 
-                    alt="" />
-                    <h3 className='text-4xl font-semibold ps-20 pb-8 -mt-16 text-white'>Salads</h3></SwiperSlide>
-                <SwiperSlide><img src={slide2} alt="" /><h3 className='text-4xl font-semibold ps-20 pb-8 -mt-16 text-white'>Pizza</h3></SwiperSlide>
-                <SwiperSlide><img src={slide3} alt="" /><h3 className='text-4xl font-semibold ps-20 pb-8 -mt-16 text-white'>Soup</h3></SwiperSlide>
-                <SwiperSlide><img src={slide4} alt="" />
-                    <h3 className='text-4xl font-semibold ps-20 pb-8 -mt-16 text-white'>Desserts</h3>
+                        alt="" />
+                    <h3 className='text-4xl font-semibold ps-20 pb-8 -mt-16 text-white'>Travel bag</h3></SwiperSlide>
+                <SwiperSlide>
+                    <img  className='h-[50vh]' src={`https://i.ibb.co/txVjJpW/julian-christian-anderson-y0t-Tb95xe-Y-unsplash.jpg`} alt="" /><h3 className='text-4xl font-semibold ps-20 pb-8 -mt-16 text-white'>
+                        Laptop bag
+                    </h3>
                 </SwiperSlide>
-                <SwiperSlide><img src={slide5} alt="" /><h3 className='text-4xl font-semibold -mt-16 ps-20 pb-8 text-white'>Salads</h3></SwiperSlide>
+                <SwiperSlide>
+                    <img  className='h-[50vh]' src={`https://i.ibb.co/1XzYpTq/2148559643.jpg`} alt="" />
+                    <h3 className='text-4xl font-semibold ps-20 pb-8 -mt-16 text-white'>
+                        School bag
+                    </h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img  className='h-[50vh]' src={`https://i.ibb.co/7Q1jfhg/jonny-caspari-5-Jx-w-Pt-Oq-JM-unsplash.jpg`} alt="" />
+                    <h3 className='text-4xl font-semibold ps-20 pb-8 -mt-16 text-white'>
+                        Sports bag
+                    </h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img  className='h-[50vh]' src={`https://i.ibb.co/cgr9S5N/2148579581.jpg`} alt="" />
+                    <h3 className='text-4xl font-semibold -mt-16 ps-20 pb-8 text-white'>
+                        Everyday bag
+                    </h3>
+                </SwiperSlide>
             </Swiper>
         </section>
     );
